96602facf05bd0eeed8fd87e20989b72ed983e97
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-03-22 01:58:44 +0700

    first commit

---
+++ src/Components/Pages/Home/Create.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Create() {
+  return (
+    <div className="homePage  ">
+      <div className="  bg-gradient-to-r from-sky-500 to-indigo-500 p-10 ">
+        <div className=" flex justify-center mb-3">
+          <div className="inputan">
+            <input
+              className=" w-auto  p-4 mr-3"
+              type="input "
+              placeholder="masukkan"
+              value=""
+            />
+          </div>
+          <div className="inputanx">
+            <button class="btn bg-teal-700 w-auto p-4 ">Button</button>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <img
+            className=" w-96"
+            src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Create;
